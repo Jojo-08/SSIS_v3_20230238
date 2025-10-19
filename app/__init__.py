941eceab0bd50179a06c8ac556f2f221fc276bf9
Jojo-08 <geromojohann@gmail.com>
2025-10-20 01:47:48 +0800

    fix: prevent circular import in database.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,17 @@
 from flask import Flask
 from config import SECRET_KEY
-from app.database import init_app
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = SECRET_KEY
 
-    init_app(app)
-    from app.controllers.user_controller import user_bp
-    from app.controllers.student_controller import student_bp
-    from app.controllers.program_controller import program_bp
-    from app.controllers.college_controller import college_bp
+    from app.database import init_app as init_db
+    init_db(app)
+    from app.controllers import user_bp, student_bp,program_bp,college_bp,main_bp
 
     app.register_blueprint(user_bp)
     app.register_blueprint(student_bp)
     app.register_blueprint(program_bp)
     app.register_blueprint(college_bp)
-    
+    app.register_blueprint(main_bp)
     return app
